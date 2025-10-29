fb6c00d2fb3b02bdd0309fe1a30e7e69f20a21c5
lexon <lexonnewb@foxmail.com>
2025-10-29 14:46:43 +0800

    feat:Implement obtain bot configuration from toml file

--- src/Config.hs
+++ src/Config.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Config (
   getDiscordSecret
+  ,loadChannelConfig
+  ,ChannelConfig(..)
 ) where
 
 import qualified Data.Text as T
 import System.Environment (lookupEnv)
+import Toml (TomlCodec,(.=),decodeFileExact)
+import qualified Toml
 
 -- | Get discord robot token with DISCORD_SECRET in environment
 getDiscordSecret :: IO String
@@ -15,8 +21,20 @@ getDiscordSecret = do
 
 data ChannelConfig = ChannelConfig
   {
-    source_channel ::[Integer]
+    source_channels ::[Integer]
     ,target_channel ::Integer
     ,max_channel :: Int
   } deriving (Show)
 
+channelConfigCodex :: TomlCodec ChannelConfig
+channelConfigCodex = ChannelConfig
+  <$> Toml.arrayOf Toml._Integer "bot.source_channels" .= source_channels
+  <*> Toml.integer "bot.target_channel" .= target_channel
+  <*> Toml.int "bot.max_channels" .= max_channel
+  
+loadChannelConfig :: FilePath -> IO ChannelConfig
+loadChannelConfig path = do
+  result <- decodeFileExact channelConfigCodex path
+  case result of
+    Left err -> ioError (userError $ "Toml decode error:\n" ++ show err)
+    Right cfg -> pure cfg
